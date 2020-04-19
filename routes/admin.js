777a4859de5c08c4527081f973744b7c8b9d794d
IsaacDSC <isaac8.silva@hotmail.com>
2020-04-19 15:40:40 -0300

    create edit-pages e edit-nav, edit-home

--- routes/admin.js
+++ routes/admin.js
@@ -4,10 +4,16 @@ const router = express.Router()
 router.get('/', (req, res) => {
     res.render('admin/dashboard', { layout: 'dashboard.handlebars' })
 })
-
 router.get('/edit-pages', (req, res) => {
-    res.render('admin/editPages', { layout: 'dashboard.handlebars' })
+    res.render('admin/edit-pages', { layout: 'dashboard.handlebars' })
 })
 
+router.get('/edit-nav', (req, res) => {
+    res.render('admin/edit-nav', { layout: 'dashboard.handlebars' })
+})
+
+router.get('/edit-home', (req, res) => {
+    res.render('admin/edit-home', { layout: 'dashboard.handlebars' })
+})
 
 module.exports = router
