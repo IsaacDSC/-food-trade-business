12f6dd5465e125fdd151b681cec0bad16686a5bf
IsaacDSC <isaac8.silva@hotmail.com>
2020-04-21 15:40:16 -0300

    adionando pagina menu para edição

--- routes/menu.js
+++ routes/menu.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.send('pagina de menus')
+    res.render('menu/menu')
 })
 
 module.exports = router
