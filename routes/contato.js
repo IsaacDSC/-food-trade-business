9c1a9e8c0c05d4190f073ecba4ac0fa5d6cd913b
IsaacDSC <isaac8.silva@hotmail.com>
2020-04-29 16:53:35 -0300

    arrumando pagina contato e pagina sobre

--- routes/contato.js
+++ routes/contato.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const router = express.Router()
 
+//add models
+const Headernav = require('../models/HeaderNav')
+const Footer = require('../models/Footer_models')
+
+
 router.get('/', (req, res) => {
-    res.render('contato/contato')
+    Headernav.findOne().then((nav) => {
+        Footer.findOne().then((footer) => {
+
+            res.render('contato/contato', { nav: nav, footer: footer })
+        })
+    })
 })
 
 module.exports = router
--- routes/sobre.js
+++ routes/sobre.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const router = express.Router()
 
+//carregando models 
+const HeaderNav = require('../models/HeaderNav')
+
+
 router.get('/', (req, res) => {
-    res.send('Pagina Sobre')
+    HeaderNav.findOne().then((nav) => {
+        res.send('Pagina Sobre', { nav: nav })
+    }).catch((err) => {
+        res.send('Error ao carregar models headernav')
+    })
 })
 
 module.exports = router
