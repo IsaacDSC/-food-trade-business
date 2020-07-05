6e845245a704162ab966c1f8905c6a51cad28307
IsaacDSC <isaac8.silva@hotmail.com>
2020-07-05 19:16:00 -0300

    upgrade

--- routes/menu.js
+++ routes/menu.js
@@ -6,18 +6,18 @@ const HeaderNav = require('../models/HeaderNav')
 const Footer = require('../models/Footer_models')
 const MenuBurger = require('../models/MenuBurger')
 const MenuBebidas = require('../models/MenuBebidas')
-const Pedidos = require('../models/Pedidos')
+    //const Pedidos = require('../models/Pedidos')
 
+var pedido = []
 
 router.get('/', (req, res) => {
     HeaderNav.findOne().then((nav) => {
         Footer.findOne().then((footer) => {
             MenuBurger.findOne().then((burger) => {
                 MenuBebidas.findOne().then((bebidas) => {
-                    Pedidos.findOne().then((pedido) => {
 
-                        res.render('menu/menu', { nav: nav, footer: footer, burger: burger, bebidas: bebidas, pedido: pedido })
-                    })
+                    res.render('menu/menu', { nav: nav, footer: footer, burger: burger, bebidas: bebidas, pedido: pedido })
+
                 })
             })
         })
@@ -37,5 +37,21 @@ router.post('/pedidos', (req, res) => {
         res.send(' error> ' + err)
     })
 })
+
+router.post('/add', (req, res) => {
+    pedido.push({
+        title: req.body.title,
+        desc: req.body.desc,
+        valor: req.body.valor
+    })
+    if (pedido == null || pedido == undefined) {
+        res.send('pedido undefined ou null')
+    } else {
+        //res.send(pedido)
+
+    }
+})
+
+
 
 module.exports = router
