6f9eaa6cfb8e98bc5c3926ea5b06fec06473eb54
IsaacDSC <isaac8.silva@hotmail.com>
2020-07-05 16:37:32 -0300

    upgrade layout

---
+++ public/js/modalmenu.js
@@ -0,0 +1,11 @@
+var titleMenu = document.querySelector('#titleMenu')
+
+function abrirMenuBebidas() {
+    document.querySelector('#modalMenu').style.display = 'block'
+}
+
+
+
+function fecharMenuBebidas() {
+    document.querySelector('#modalMenu').style.display = 'none'
+}
