a4795c5dd5b191e7f19b7fcdd610def0cfd1bc34
IsaacDSC <isaac8.silva@hotmail.com>
2020-07-07 13:48:43 -0300

    upgrade menu

--- routes/menu.js
+++ routes/menu.js
@@ -11,6 +11,11 @@ const MenuPizzas = require('../models/MenuPizza')
 
 var pedido = []
 
+for (var i = 1; i < pedido; i++) {
+    console.log(pedido)
+}
+
+
 router.get('/', (req, res) => {
     HeaderNav.findOne().then((nav) => {
         Footer.findOne().then((footer) => {
@@ -20,11 +25,24 @@ router.get('/', (req, res) => {
         })
     })
 })
+
+router.post('/add', (req, res) => {
+    //var quantidade = pedido.length + 1
+    pedido.push({ title: req.body.title, valor: req.body.valor })
+    if (pedido == null || pedido == undefined) {
+        res.send('pedido undefined ou null')
+    } else {
+        //res.send(pedido)        
+        res.redirect('/menu/bebidas')
+    }
+})
+
 router.get('/bebidas', (req, res) => {
     HeaderNav.findOne().then((nav) => {
         Footer.findOne().then((footer) => {
             MenuBebidas.findOne().then((bebidas) => {
-                res.render('menu/bebidas', { layout: 'menu.handlebars', nav: nav, footer: footer, bebidas: bebidas, pedido: pedido })
+                const total =
+                    res.render('menu/bebidas', { layout: 'menu.handlebars', nav: nav, footer: footer, bebidas: bebidas, pedido: pedido })
 
             })
         })
@@ -44,21 +62,7 @@ router.get('/pizza', (req, res) => {
 
 
 
-router.post('/add', (req, res) => {
-    pedido.push({
-        title: req.body.title,
-        desc: req.body.desc,
-        valor: req.body.valor
-    })
-    if (pedido == null || pedido == undefined) {
-        res.send('pedido undefined ou null')
-    } else {
-        //res.send(pedido)
-        res.redirect('/menu')
-            //console.log(pedido[0]['title'])
 
-    }
-})
 
 
 
