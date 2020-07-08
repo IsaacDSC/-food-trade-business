09870bd596b5b6dd4af5513de14c27ce86ea6755
IsaacDSC <isaac8.silva@hotmail.com>
2020-07-08 11:17:26 -0300

    upgrades

--- routes/menu.js
+++ routes/menu.js
@@ -9,31 +9,31 @@ const MenuBebidas = require('../models/MenuBebidas')
 const MenuPizzas = require('../models/MenuPizza')
     //const Pedidos = require('../models/Pedidos')
 
-var pedido = []
+var pedidos = []
 
-for (var i = 1; i < pedido; i++) {
-    console.log(pedido)
-}
 
+const SubTotal = pedidos.reduce((subtotal, pedido) => pedido.valor + subtotal, 0)
 
 router.get('/', (req, res) => {
     HeaderNav.findOne().then((nav) => {
         Footer.findOne().then((footer) => {
             MenuBurger.findOne().then((burger) => {
-                res.render('menu/burger', { layout: 'menu.handlebars', nav: nav, footer: footer, burger: burger, pedido: pedido })
+                res.render('menu/burger', { layout: 'menu.handlebars', nav: nav, footer: footer, burger: burger, pedidos: pedidos, SubTotal: SubTotal })
             })
         })
     })
 })
 
+
 router.post('/add', (req, res) => {
+
     //var quantidade = pedido.length + 1
-    pedido.push({ title: req.body.title, valor: req.body.valor })
-    if (pedido == null || pedido == undefined) {
+    pedidos.push({ title: req.body.title, valor: req.body.valor })
+    if (pedidos == null || pedidos == undefined) {
         res.send('pedido undefined ou null')
     } else {
         //res.send(pedido)        
-        res.redirect('/menu/bebidas')
+        res.redirect('/menu')
     }
 })
 
@@ -41,8 +41,7 @@ router.get('/bebidas', (req, res) => {
     HeaderNav.findOne().then((nav) => {
         Footer.findOne().then((footer) => {
             MenuBebidas.findOne().then((bebidas) => {
-                const total =
-                    res.render('menu/bebidas', { layout: 'menu.handlebars', nav: nav, footer: footer, bebidas: bebidas, pedido: pedido })
+                res.render('menu/bebidas', { layout: 'menu.handlebars', nav: nav, footer: footer, bebidas: bebidas, pedidos: pedidos, SubTotal: SubTotal })
 
             })
         })
